a94c94b6ab5009d82878e7c64a7f4e1bc42ad3e7
Jairo Soares <jairo.soares@gmail.com>
2025-01-18 21:07:50 -0300

    feat:class 41,42,43,44,45,46,47

--- src/main/java/academy/jairo/springboot/springcrud/dto/LessonDTO.java
+++ src/main/java/academy/jairo/springboot/springcrud/dto/LessonDTO.java
@@ -1,6 +1,5 @@
 package academy.jairo.springboot.springcrud.dto;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/academy/jairo/springboot/springcrud/enums/Category.java
+++ src/main/java/academy/jairo/springboot/springcrud/enums/Category.java
@@ -6,7 +6,7 @@ public enum Category {
 
     private final String value;
 
-    private Category(String value) {
+    Category(String value) {
         this.value = value;
     }
 
